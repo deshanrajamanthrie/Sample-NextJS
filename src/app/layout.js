c56d8de0766c26e3e2764c804d3330cc062386aa
Deshan Rajamanthrie <deshanrajamanthrie2001@gmail.com>
2024-02-29 10:33:02 +0530

    add the some comments to the layout file

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,9 @@
 import {Inter } from "next/font/google";
-import "./globals.css";
+import "./globals.css"; // adding the global css to all files  
 
 const inter =Inter({ subsets: ["latin"] , weight:'variable'});// pass the by default
 
-export const metadata = {
+export const metadata = { // meta data
   title: "Mcode",
   description: "I want to create a new Sample application",
 };
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}</body> {/* root eke ati page.jsx ek refer  */}
     </html>
   );
 }
